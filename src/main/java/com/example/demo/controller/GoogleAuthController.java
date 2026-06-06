ce62a2bc6846370133c1c38482194028daddc565
Pravinkumar <sensaipravin@gmail.com>
2026-06-07 03:42:59 +0530

    Configure CORS to allow dynamic public origins for LocalTunnel

--- src/main/java/com/example/demo/controller/GoogleAuthController.java
+++ src/main/java/com/example/demo/controller/GoogleAuthController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class GoogleAuthController {
 
  @Value("${google.client-id}")
